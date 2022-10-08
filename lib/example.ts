a302c688467331e9e1f261df3698fe62998d1f3f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-10-08 10:17:38 +0200

    Create class for example

---
+++ lib/example.ts
@@ -0,0 +1,26 @@
+
+export class Something {
+
+    constructor(length: number, width: number) {
+        this.#length = length;
+        this.#width = width;
+    }
+
+    #length: number;
+    #width: number;
+
+    get length() { return this.#length; }
+    set length(l: number) { this.#length = l; }
+
+    get width() { return this.#width; }
+    set(w: number) { this.#width = w; }
+
+    area() { return this.#length * this.#width; }
+    ratio1() { return this.#length / this.#width; }
+    ratio2() { return this.#width / this.#length; }
+
+}
+
+export function mkSomething(length: number, width: number) {
+    return new Something(length, width);
+}
